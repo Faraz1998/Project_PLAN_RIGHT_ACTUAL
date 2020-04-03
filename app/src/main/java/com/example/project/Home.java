255bcfb046db0b3218c769b1f107f4cfa5a53812
Faraz Hafeez <21713096@bucks.ac.uk>
2020-04-03 19:54:09 +0100

    added a side navigation

--- app/src/main/java/com/example/project/Home.java
+++ app/src/main/java/com/example/project/Home.java
@@ -1,14 +1,32 @@
 package com.example.project;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
-
+public DrawerLayout dl;
+public ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        dl = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, dl, R.string.open, R.string.close);
+        dl.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
